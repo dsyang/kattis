5a0b07b5d695b480880f906ba99e94ed2a23b7b2
Dan Yang <dsyang92@gmail.com>
2019-03-14 10:03:26 -0700

    solve quadrant

---
+++ quadrant/src/main.rs
@@ -0,0 +1,14 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+    let x = lines.next().unwrap().unwrap().trim().parse::<i64>().unwrap();
+    let y = lines.next().unwrap().unwrap().trim().parse::<i64>().unwrap();
+    match (x > 0,y > 0) {
+        (true, true) => println!("1"),
+        (false, true) => println!("2"),
+        (false, false) => println!("3"),
+        (true, false) => println!("4")
+    }
+}
