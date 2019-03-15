cd0a7a084478b856ca9240a3b07f3a8a46608692
Dan Yang <dsyang92@gmail.com>
2019-03-14 20:15:46 -0700

    solve

---
+++ faktor/src/main.rs
@@ -0,0 +1,11 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    let mut input = String::new();
+    if let Ok(_) = stdin.read_line(&mut input) {
+        let v = input.split(' ').map(|s| s.trim().parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        let ans :i64 = v[0] * (v[1]-1) + 1;
+        println!("{}", ans);
+    }
+}
