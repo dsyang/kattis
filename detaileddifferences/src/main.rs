48306bad5076c6bfe59a0cc1e8018ebe3b5e9096
Dan Yang <dsyang92@gmail.com>
2019-03-14 21:41:58 -0700

    solve detaileddifferences

---
+++ detaileddifferences/src/main.rs
@@ -0,0 +1,15 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let mut input = String::new();
+    if let Ok(_) = io::stdin().read_line(&mut input) {
+        let num = input.trim().parse::<i64>().unwrap();
+        let stdin = io::stdin();
+        for _ in 0..num {
+            let mut lines = stdin.lock().lines().take(2);
+            let line1 = String::from(lines.next().unwrap().unwrap().trim());
+            let line2 = String::from(lines.next().unwrap().unwrap().trim());
+            println!("{}\n{}\n{}\n", line1, line2, line1.chars().zip(line2.chars()).map(|(a,b)| if a==b {'.'} else { '*'}).collect::<String>())
+        }
+    }
+}
