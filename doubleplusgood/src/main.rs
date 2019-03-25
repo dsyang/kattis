b983c32699fe0b05c49aed2d74ce665df45f45a8
Dan Yang <dsyang92@gmail.com>
2019-03-25 16:40:17 -0700

    begin double plus

--- doubleplusgood/src/main.rs
+++ doubleplusgood/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{self};
+use std::collections::HashSet;
+
+
+fn compute(numbers : Vec<i64>, solutions: &mut HashSet<i64>) {
+    panic!()
+}
 fn main() {
-    println!("Hello, world!");
+    let mut solutions : HashSet<i64> = HashSet::new();
+    let mut input = String::new();
+    let stdin = io::stdin();
+    if let Ok(_) = stdin.read_line(&mut input) {
+        let numbers : Vec<i64> = input.trim().split('+').map(|x| x.parse::<i64>().unwrap()).collect();
+        compute(numbers, &mut solutions);
+    }
+    
+    println!("{}", solutions.len());
 }
